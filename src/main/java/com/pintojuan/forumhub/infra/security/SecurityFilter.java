2a3fbb2856c8f13c27846c0f0362f7dc1ae58ab1
PintoJuan <juanmartinpinto30@gmail.com>
2025-08-19 15:08:14 -0300

    Verificamos que nuestro token haya sido creado por nosotros

---
+++ src/main/java/com/pintojuan/forumhub/infra/security/SecurityFilter.java
@@ -0,0 +1,34 @@
+package com.pintojuan.forumhub.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+        var subject = tokenService.getSubject(tokenJWT);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorization = request.getHeader("Authorization");
+        if (authorization == null) {
+            throw new RuntimeException("Token no enviado en el encabezado de Authorization");
+        }
+        return authorization.replace("Bearer ", "");
+    }
+}
--- src/main/java/com/pintojuan/forumhub/infra/security/TokenService.java
+++ src/main/java/com/pintojuan/forumhub/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.pintojuan.forumhub.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.pintojuan.forumhub.domain.usuarios.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -32,5 +33,18 @@ public class TokenService {
 
     private Instant fechaExpiracion() {
         return LocalDateTime.now().plusHours(12).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Forum")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT invalido o expirado");
+        }
     }
 }
